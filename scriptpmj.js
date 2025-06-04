6d40cd5044b337dfffbd988087313b3fc6e1d8fc
Lauro Goncalves <201910646@al.facamp.com.br>
2025-06-04 01:36:32 -0300

    Update scriptpmj.js

--- scriptpmj.js
+++ scriptpmj.js
@@ -1,4 +1,4 @@
-console.log("V1.0");
+console.log("V1.01");
 $(document).ready(function() {
 	setTimeout(function() {
 		document.getElementById('initial-loading').style.display = 'none';
@@ -121,8 +121,6 @@ $(document).ready(function() {
 		
 		let bbcode = gerarBBCode(formType);
 		
-		$('#fa-generated-message').val(bbcode);
-		
 		$(this).text('Enviando...');
 		const btn = $(this); 
 		
@@ -142,7 +140,7 @@ $(document).ready(function() {
 				
 				btn.text('Enviar');
 				}).fail(function () {
-				console.log("BBCode gerado:", $('#fa-generated-message').val().trim());
+				console.log("BBCode gerado:", bbcode);
 				document.body.removeChild(loadingOverlay);
 				$('#confirmation-modal').addClass('show');
 				$('#confirmation-modal').css({
@@ -154,141 +152,183 @@ $(document).ready(function() {
 		}, 600);
 	});
 	
-	function gerarBBCode(formType) {
-		let titulo = '';
-		let nome = '';
+	const BBCodeTemplates = {
+		header: '[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]{{titulo}}[/b][/color][/center][/size][/font][/td][/tr][/table]',
 		
-		switch(formType) {
-			case 'entrada':
-			titulo = 'ENTRADA DE MEMBROS';
-			nome = $('#entrada-nome').val();
-			const dataEntrada = $('#entrada-data').val();
-			const dataEntradaFormatada = formatarData(dataEntrada);
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Data[/b][/color]: ${dataEntradaFormatada} [/center][/font][/size]`;
-			break;
-			
-			case 'promocao':
-			titulo = 'PROMOÇÃO DE MEMBRO';
-			nome = $('#promocao-nome').val();
-			const cargoAtualProm = $('#promocao-cargo-atual').val();
-			const cargoNovoProm = $('#promocao-novo-cargo').val();
-			const motivoProm = $('#promocao-motivo').val();
-			const dataProm = formatarData($('#promocao-data').val());
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Cargo Atual[/b][/color]: ${cargoAtualProm}
-			[color=black][b]Novo Cargo[/b][/color]: ${cargoNovoProm}
-			[color=black][b]Motivo[/b][/color]: ${motivoProm}
-			[color=black][b]Data[/b][/color]: ${dataProm} [/center][/font][/size]`;
-			break;
-			
-			case 'rebaixamento':
-			titulo = 'REBAIXAMENTO DE MEMBRO';
-			nome = $('#rebaixamento-nome').val();
-			const cargoAtualReb = $('#rebaixamento-cargo-atual').val();
-			const novoCargoReb = $('#rebaixamento-novo-cargo').val();
-			const motivoReb = $('#rebaixamento-motivo').val();
-			const dataReb = $('#rebaixamento-data').val();
-			const dataRebFormatada = formatarData(dataReb);
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Cargo Atual[/b][/color]: ${cargoAtualReb}
-			[color=black][b]Novo Cargo[/b][/color]: ${novoCargoReb}
-			[color=black][b]Motivo[/b][/color]: ${motivoReb}
-			[color=black][b]Data[/b][/color]: ${dataRebFormatada} [/center][/font][/size]`;
-			break;
-			
-			case 'expulsao':
-			titulo = 'EXPULSÃO DE MEMBRO';
-			nome = $('#expulsao-nome').val();
-			const motivoExp = $('#expulsao-motivo').val();
-			const comprovacoesExp = $('#expulsao-comprovacoes').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Motivo[/b][/color]: ${motivoExp}
-			[color=black][b]Comprovações[/b][/color]: ${comprovacoesExp} [/center][/font][/size]`;
-			break;
-			
-			case 'advertencia':
-			titulo = 'ADVERTÊNCIA';
-			nome = $('#advertencia-nome').val();
-			const motivoAdv = $('#advertencia-motivo').val();
-			const permissaoAdv = $('#advertencia-permissao').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Motivo[/b][/color]: ${motivoAdv}
-			[color=black][b]Permissão[/b][/color]: ${permissaoAdv} [/center][/font][/size]`;
-			break;
-			
-			case 'licenca':
-			titulo = 'LICENÇA / RESERVA';
-			nome = $('#licenca-nome').val();
-			const diasLic = $('#licenca-dias').val();
-			const permissaoLic = $('#licenca-permissao').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Quantidade de Dias[/b][/color]: ${diasLic}
-			[color=black][b]Permissão[/b][/color]: ${permissaoLic} [/center][/font][/size]`;
-			break;
-			
-			case 'saida':
-			titulo = 'SAÍDA DE MEMBRO';
-			nome = $('#saida-nome').val();
-			const motivoSaida = $('#saida-motivo').val();
-			const permissaoSaida = $('#saida-permissao').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Motivo[/b][/color]: ${motivoSaida}
-			[color=black][b]Permissão[/b][/color]: ${permissaoSaida} [/center][/font][/size]`;
-			break;
-			
-			case 'prolongamento':
-			titulo = 'PROLONGAMENTO DE LICENÇA';
-			nome = $('#prolongamento-nome').val();
-			const diasProl = $('#prolongamento-dias').val();
-			const permissaoProl = $('#prolongamento-permissao').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Quantidade de Dias[/b][/color]: ${diasProl}
-			[color=black][b]Permissão[/b][/color]: ${permissaoProl} [/center][/font][/size]`;
-			break;
-			
-			case 'retorno':
-			titulo = 'RETORNO DE LICENÇA / RESERVA';
-			nome = $('#retorno-nome').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[/center][/font][/size]`;
-			break;
-			
-			case 'alteracao':
-			titulo = 'ALTERAÇÃO DE NICKNAME';
-			nome = $('#alteracao-nome-antigo').val();
-			const cargoAlt = $('#alteracao-cargo').val();
-			const nomeNovo = $('#alteracao-nome-novo').val();
-			var bbcode = `[font=Poppins][center][table style="border-color: black; border-radius: 10px; overflow: hidden; width: auto;" bgcolor="#4d4c4c"][tr][td][size=16][color=#ffffff][b]${titulo}[/b][/color][/center][/size][/font][/td][/tr][/table]
-			
-			[center][size=14][font=Poppins][b][color=black]${nome}[/color][/b] 
-			[color=black][b]Cargo[/b][/color]: ${cargoAlt}
-			[color=black][b]Novo Nome[/b][/color]: ${nomeNovo} [/center][/font][/size]`;
-			break;
-			
-			case 'atualizacao':
-			const tag = $('#atualizacao-nome').val();
-			var bbcode = `<table class="rank pmj" style="transition: none 0ms ease 0s; margin: 1em; padding: 1.4em; RCC - SÓ A VERDADEIRA-color: rgb(0, 92, 3); width: -webkit-fill-available; height: auto; text-align: center; border-radius: 8px; color: white; border-collapse: collapse; font-family: Roboto, sans-serif; border-width: initial !important; border-style: none !important; border-color: initial !important;"><tbody><tr style="transition: none; border: none; box-sizing: border-box; margin: 0px; padding: 0px;"><td style="transition: none 0ms ease 0s; box-sizing: border-box; margin: 0px; padding: 15px; border-width: initial !important; border-style: none !important; border-color: initial !important;"><img src="https://www.habbo.com.br/habbo-imaging/badge/b09244s43131s50134s17133s17135b1210d8727f4f7f0adf08ed5ab5bd644.gif" alt="PMJ LOGO"><br><font face="Poppins"><font color="white"><span style="font-size: 17px; line-height: normal"><strong>Atualizado por ${tag}</strong></span></font></font></td></tr></tbody></table>`;
-			break;
+		content: '[center][size=14][font=Poppins][b][color=black]{{nome}}[/color][/b]{{campos}}[/center][/font][/size]',
+		fields: {
+			data: '\n[color=black][b]Data[/b][/color]: {{valor}}',
+			cargoAtual: '\n[color=black][b]Cargo Atual[/b][/color]: {{valor}}',
+			novoCargo: '\n[color=black][b]Novo Cargo[/b][/color]: {{valor}}',
+			motivo: '\n[color=black][b]Motivo[/b][/color]: {{valor}}',
+			comprovacoes: '\n[color=black][b]Comprovações[/b][/color]: {{valor}}',
+			permissao: '\n[color=black][b]Permissão[/b][/color]: {{valor}}',
+			quantidadeDias: '\n[color=black][b]Quantidade de Dias[/b][/color]: {{valor}}',
+			cargo: '\n[color=black][b]Cargo[/b][/color]: {{valor}}',
+			novoNome: '\n[color=black][b]Novo Nome[/b][/color]: {{valor}}'
+		},
+		
+		atualizacao: '<table class="rank pmj" style="transition: none 0ms ease 0s; margin: 1em; padding: 1.4em; RCC - SÓ A VERDADEIRA-color: rgb(0, 92, 3); width: -webkit-fill-available; height: auto; text-align: center; border-radius: 8px; color: white; border-collapse: collapse; font-family: Roboto, sans-serif; border-width: initial !important; border-style: none !important; border-color: initial !important;"><tbody><tr style="transition: none; border: none; box-sizing: border-box; margin: 0px; padding: 0px;"><td style="transition: none 0ms ease 0s; box-sizing: border-box; margin: 0px; padding: 15px; border-width: initial !important; border-style: none !important; border-color: initial !important;"><img src="https://www.habbo.com.br/habbo-imaging/badge/b09244s43131s50134s17133s17135b1210d8727f4f7f0adf08ed5ab5bd644.gif" alt="PMJ LOGO"><br><font face="Poppins"><font color="white"><span style="font-size: 17px; line-height: normal"><strong>Atualizado por {{tag}}</strong></span></font></font></td></tr></tbody></table>'
+	};
+	
+	const FormConfigs = {
+		entrada: {
+			titulo: 'ENTRADA DE MEMBROS',
+			campos: ['data']
+		},
+		promocao: {
+			titulo: 'PROMOÇÃO DE MEMBRO',
+			campos: ['cargoAtual', 'novoCargo', 'motivo', 'data']
+		},
+		rebaixamento: {
+			titulo: 'REBAIXAMENTO DE MEMBRO',
+			campos: ['cargoAtual', 'novoCargo', 'motivo', 'data']
+		},
+		expulsao: {
+			titulo: 'EXPULSÃO DE MEMBRO',
+			campos: ['motivo', 'comprovacoes']
+		},
+		advertencia: {
+			titulo: 'ADVERTÊNCIA',
+			campos: ['motivo', 'permissao']
+		},
+		licenca: {
+			titulo: 'LICENÇA / RESERVA',
+			campos: ['quantidadeDias', 'permissao']
+		},
+		saida: {
+			titulo: 'SAÍDA DE MEMBRO',
+			campos: ['motivo', 'permissao']
+		},
+		prolongamento: {
+			titulo: 'PROLONGAMENTO DE LICENÇA',
+			campos: ['quantidadeDias', 'permissao']
+		},
+		retorno: {
+			titulo: 'RETORNO DE LICENÇA / RESERVA',
+			campos: []
+		},
+		alteracao: {
+			titulo: 'ALTERAÇÃO DE NICKNAME',
+			campos: ['cargo', 'novoNome']
 		}
+	};
+
+	function processTemplate(template, data) {
+		return template.replace(/\{\{(\w+)\}\}/g, function(match, key) {
+			return data[key] || match;
+		});
+	}
+	
+	function obterDadosFormulario(formType) {
+		const dados = {};
+		
+		const fieldMappings = {
+			entrada: {
+				nome: 'entrada-nome',
+				data: 'entrada-data'
+			},
+			promocao: {
+				nome: 'promocao-nome',
+				cargoAtual: 'promocao-cargo-atual',
+				novoCargo: 'promocao-novo-cargo',
+				motivo: 'promocao-motivo',
+				data: 'promocao-data'
+			},
+			rebaixamento: {
+				nome: 'rebaixamento-nome',
+				cargoAtual: 'rebaixamento-cargo-atual',
+				novoCargo: 'rebaixamento-novo-cargo',
+				motivo: 'rebaixamento-motivo',
+				data: 'rebaixamento-data'
+			},
+			expulsao: {
+				nome: 'expulsao-nome',
+				motivo: 'expulsao-motivo',
+				comprovacoes: 'expulsao-comprovacoes'
+			},
+			advertencia: {
+				nome: 'advertencia-nome',
+				motivo: 'advertencia-motivo',
+				permissao: 'advertencia-permissao'
+			},
+			licenca: {
+				nome: 'licenca-nome',
+				quantidadeDias: 'licenca-dias',
+				permissao: 'licenca-permissao'
+			},
+			saida: {
+				nome: 'saida-nome',
+				motivo: 'saida-motivo',
+				permissao: 'saida-permissao'
+			},
+			prolongamento: {
+				nome: 'prolongamento-nome',
+				quantidadeDias: 'prolongamento-dias',
+				permissao: 'prolongamento-permissao'
+			},
+			retorno: {
+				nome: 'retorno-nome'
+			},
+			alteracao: {
+				nome: 'alteracao-nome-antigo',
+				cargo: 'alteracao-cargo',
+				novoNome: 'alteracao-nome-novo'
+			},
+			atualizacao: {
+				tag: 'atualizacao-nome'
+			}
+		};
+		
+		const mapping = fieldMappings[formType];
+		if (!mapping) return dados;
+
+		Object.keys(mapping).forEach(key => {
+			const elementId = mapping[key];
+			const element = document.getElementById(elementId);
+			if (element) {
+				let valor = element.value;
+				if (key === 'data' && valor) {
+					valor = formatarData(valor);
+				}
+				
+				dados[key] = valor;
+			}
+		});
+		
+		return dados;
+	}
+
+	function gerarBBCode(formType) {
+
+		if (formType === 'atualizacao') {
+			const dados = obterDadosFormulario(formType);
+			return processTemplate(BBCodeTemplates.atualizacao, dados);
+		}
+
+		const config = FormConfigs[formType];
+		if (!config) {
+			console.error('Tipo de formulário não encontrado:', formType);
+			return '';
+		}
+
+		const dados = obterDadosFormulario(formType);
+		dados.titulo = config.titulo;
+		
+		let bbcode = processTemplate(BBCodeTemplates.header, dados);
+		bbcode += '\n\n';
+		
+		let campos = '';
+		config.campos.forEach(campo => {
+			if (dados[campo] && BBCodeTemplates.fields[campo]) {
+				const campoTemplate = BBCodeTemplates.fields[campo];
+				campos += processTemplate(campoTemplate, { valor: dados[campo] });
+			}
+		});
+		
+		dados.campos = campos + ' ';
+		bbcode += processTemplate(BBCodeTemplates.content, dados);
+		
 		return bbcode;
 	}
 	
