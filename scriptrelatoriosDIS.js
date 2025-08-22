60d9cd98af09ca4857a174739d4de1736197b673
Lauro Goncalves <201910646@al.facamp.com.br>
2025-08-22 02:53:57 -0300

    Update scriptrelatoriosDIS.js

--- scriptrelatoriosDIS.js
+++ scriptrelatoriosDIS.js
@@ -1,4 +1,4 @@
-console.log("V1.0");
+console.log("V1.01");
 document.addEventListener('DOMContentLoaded', function() {
             const btn = document.getElementById('btnAdvertencias');
             if (btn) {
@@ -95,7 +95,7 @@ function renderAdvertenciasTable(csvText) {
         let changeLog = [];
         
         const SPREADSHEET_ID = '1vSMZhcsyhDINjmQHSsuz4bPWeKFCFEDMBfTDjlDFlTZKFiOd6ZlmVjznD1fiRoj9kkRfmfNcMnlKArz';
-        const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw-h_j6GZLklojmAhSJGsLXkEow6ZQKNy5oivYJXpHqS2KYbdTh_JLkjOVPVFvtJw15/exec';
+        const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwjL8YzsBzdUo2h5jJLL-7LOyFAzv38zwYYHgJaFIdptmXTEsIspe3Kedb0Svz99Hrj/exec';
         const LOG_URL = 'https://centraldecasos-dis.rf.gd/logs.txt';
         const LOG_PHP_URL = './salvarlog.php';
         
